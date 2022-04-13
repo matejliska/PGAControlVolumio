f7bde5c3597406cf46e6c5b87b71b31b8e6ca088
matejliska <45861507+matejliska@users.noreply.github.com>
2022-04-13 17:06:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,38 @@ var spawn = require('child_process').spawn
 
 const Gpio = require('onoff').Gpio;
 const io = require('socket.io-client');
-const SPI = require('pi-spi');
+var SPI = require('pi-spi');
+const Rotary = require('raspberrypi-rotary-encoder');
 
 var async = require('async');
 var u = require('lodash');
 var fs = require('fs');
 
+
+const pinClk = 0;
+const pinDt = 1;
+const pinSwitch = 2;  // Optional switch
+
+const rotary = new Rotary(pinClk, pinDt, pinSwitch);
+
+    rotary.on("rotate", (delta) => {
+      console.log("Rotation :"+delta);
+    });
+    rotary.on("pressed", () => {
+      console.log("Rotary switch pressed");
+    });
+    rotary.on("released", () => {
+      console.log("Rotary switch released");
+    });
+
+var spi = SPI.initialize("/dev/spidev0.0"),
+    test = Buffer.from("Hello, World!");
+
+
+const rotaryTypes = new Array(
+	"...",
+	"1/1",
+	"1/2",
+	"...",
+	"1/4"
+);
