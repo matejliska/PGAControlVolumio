9832bd7a54755a0bf5424bf5c33b6224219d1f69
matejliska <45861507+matejliska@users.noreply.github.com>
2022-04-20 19:54:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var libQ = require('kew');
 var fs=require('fs-extra');
 const path=require('path');
 var exec = require('child_process').exec;
-var spawn = require('child_process').spawn
+var spawn = require('child_process').spawn;
 
 const Gpio = require('onoff').Gpio;
 const io = require('socket.io-client');
@@ -22,8 +22,25 @@ const pinSwitch = 3;  // Optional switch
 
 const rotary = new Rotary(pinClk, pinDt, pinSwitch);
 
+var spi = SPI.initialize("/dev/spidev0.0"),
+    test = Buffer.from("Hello, World!");
+
+
     rotary.on("rotate", (delta) => {
       console.log("Rotation :"+delta);
+        spi.write(test, function (e,d) {
+            if (e) console.error(e);
+            else console.log("Got \""+d.toString()+"\" back.");
+
+            if (test.toString() === d.toString()) {
+                console.log(msg);
+            } else {
+                // NOTE: this will likely happen unless MISO is jumpered to MOSI
+                console.warn(msg);
+                process.exit(-2);
+            }
+        });
+
     });
     rotary.on("pressed", () => {
       console.log("Rotary switch pressed");
@@ -32,8 +49,6 @@ const rotary = new Rotary(pinClk, pinDt, pinSwitch);
       console.log("Rotary switch released");
     });
 
-var spi = SPI.initialize("/dev/spidev0.0"),
-    test = Buffer.from("Hello, World!");
 
 spi.transfer(test, test.length, function (e,d) {
     if (e) console.error(e);
