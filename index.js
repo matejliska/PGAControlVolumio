d5f34c807e217650b368507439ad801024ba1a23
matejliska <45861507+matejliska@users.noreply.github.com>
2022-05-19 15:47:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,131 @@
 'use strict';
 
-var libQ = require('kew');
-var fs=require('fs-extra');
-const path=require('path');
+var fs = require('fs-extra');
+var libFsExtra = require('fs-extra');
 var exec = require('child_process').exec;
-var spawn = require('child_process').spawn;
+var execSync = require('child_process').execSync;
+var libQ = require('kew');
+var config = new (require('v-conf'))();
 
-const Gpio = require('onoff').Gpio;
-const io = require('socket.io-client');
 var SPI = require('pi-spi');
-const Rotary = require('raspberrypi-rotary-encoder');
-
-var async = require('async');
-var u = require('lodash');
-var fs = require('fs');
+var spi = SPI.initialize("/dev/spidev0.0");
 
 
-const pinClk = 4; //GPIO 23
-const pinDt = 5; //GPIO 24
-const pinSwitch = 3;  // Optional switch
+module.exports = SPIvolumecontrol;
 
-const rotary = new Rotary(pinClk, pinDt, pinSwitch);
+function SPIvolumecontrol(context) {
+  var self = this;
+  self.context = context;
+  self.commandRouter = self.context.coreCommand;
+  self.logger = self.context.logger;
 
-var spi = SPI.initialize("/dev/spidev0.0"),
-    test = Buffer.from("Hello, World!");
+  this.configManager = this.context.configManager;
+};
+
+SPIvolumecontrol.prototype.onVolumioStart = function () {
+  var self = this;
+  var configFile = this.commandRouter.pluginManager.getConfigurationFile(this.context, 'config.json');
+  this.config = new (require('v-conf'))();
+  this.config.loadFile(configFile);
+  return libQ.resolve();
+};
 
 
-    rotary.on("rotate", (delta) => {
-      console.log("Rotation :"+delta);
-        spi.write(test, function (e,d) {
-            if (e) console.error(e);
-            else console.log("Got \""+d.toString()+"\" back.");
+SPIvolumecontrol.prototype.getConfigurationFiles = function()
+{
+	return ['config.json'];
+}
 
-            if (test.toString() === d.toString()) {
-                console.log(msg);
-            } else {
-                // NOTE: this will likely happen unless MISO is jumpered to MOSI
-                console.warn(msg);
-                process.exit(-2);
-            }
-        });
 
+SPIvolumecontrol.prototype.onStart = function () {
+  var self = this;
+  var defer = libQ.defer();
+    self.logger.info('SPIvolumecotrol Started');
+  return defer.promise;
+};
+
+
+SPIvolumecontrol.prototype.onRestart = function () {
+  var self = this;
+};
+
+SPIvolumecontrol.prototype.onInstall = function () {
+  var self = this;
+  //	//Perform your installation tasks here
+};
+
+SPIvolumecontrol.prototype.onUninstall = function () {
+  var self = this;
+  //Perform your installation tasks here
+};
+
+SPIvolumecontrol.prototype.getUIConfig = function () {
+  var self = this;
+  var defer = libQ.defer();
+  var lang_code = this.commandRouter.sharedVars.get('language_code');
+  self.commandRouter.i18nJson(__dirname + '/i18n/strings_' + lang_code + '.json',
+    __dirname + '/i18n/strings_en.json',
+    __dirname + '/UIConfig.json')
+    .then(function (uiconf) {
+
+      uiconf.sections[0].content[0].config.bars[0].value = self.config.get('mg');
+   defer.resolve(uiconf);
+    })
+    .fail(function () {
+      defer.reject(new Error());
     });
-    rotary.on("pressed", () => {
-      console.log("Rotary switch pressed");
-    });
-    rotary.on("released", () => {
-      console.log("Rotary switch released");
-    });
+  return defer.promise;
+};
 
 
-spi.transfer(test, test.length, function (e,d) {
-    if (e) console.error(e);
-    else console.log("Got \""+d.toString()+"\" back.");
+SPIvolumecontrol.prototype.volumeChanged = function (data) {
+  var self = this;
+
+  var defer = libQ.defer();
+
+  self.config.set('mg', data['mg']);
+  
+  self.logger.info('Configurations of equalizer have been set');
+
+  self.rebuildvolSPI()
+    .then(function (e) {
+      //  self.commandRouter.pushToastMessage('success', "Bauer Configuration updated");
+      defer.resolve({});
+    })
+    .fail(function (e) {
+      defer.reject(new Error('error'));
+      //  self.commandRouter.pushToastMessage('error', "failed to start. Check your config !");
+    })
+
+
+  return defer.promise;
+
+};
+
+SPIvolumecontrol.prototype.rebuildvolSPI = function () {
+  var self = this;
+  var defer = libQ.defer();
     
-    if (test.toString() === d.toString()) {
-        console.log(msg);
-    } else {
-        // NOTE: this will likely happen unless MISO is jumpered to MOSI
-        console.warn(msg);
-        process.exit(-2);
-    }
-});
+    var outputLR = parseInt(self.config.get('mg'));
+    outputLR += 100;
+    var outSub = outputLR + 15;
+    var test = Buffer.from(outSub, outSub, outputLR, outputLR)
 
+      spi.transfer(test, test.length, function (e,d) {
+        if (e) console.error(e);
+        else console.log("Got \""+d.toString()+"\" back.");
 
+        if (test.toString() === d.toString()) {
+            self.logger.info(msg);
+        } else {
+            // NOTE: this will likely happen unless MISO is jumpered to MOSI
+            self.logger.info(msg);
+            process.exit(-2);
+        }
+       });
+  
 
+    
+  return defer.promise;
+  
+};
